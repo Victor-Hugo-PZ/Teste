71e2839f163a219e9499f3205efcd2241cba5d80
Tetola-17 <vhpzakimi@hotmail.com>
2022-02-19 01:19:14 -0300

    teste4

---
+++ prog web 2/aula 01/js/app.js
@@ -0,0 +1,69 @@
+
+
+
+
+
+
+
+
+
+// console do browser-
+// mostra oq estamos pegando na página
+// mostra resultado de processamento
+// mostra erros
+
+console.log('SALVE SALVE FAMIL');
+console.log('Manda o papo');
+
+// variáveis - Não tem tipo
+// JS faz tipagem automática - conforme o valor recebido
+
+let nomeUsuario = 'Tetola Jogador';
+let emailUsuario = 'vhpzakimi@gmail.com';
+let idadeUsuario = 26;
+let salarioUsuario = 5000.69;
+
+let loginUsuario = false;   //    \
+let contaUsuario;           //     ]->  todas são iguais e tem o valor vazio
+let statusUsuario = null    //    /
+
+
+//    template string - começar e terminar com crase
+//    Para chamar as variáveis usar:  ${variável}
+
+console.log(`Usuário: ${nomeUsuario}
+Email: ${emailUsuario}`);
+
+
+//   Exibindo o tipo atribuído 
+console.log(typeof nomeUsuario);
+console.log(typeof emailUsuario);
+console.log(typeof idadeUsuario);
+console.log(typeof salarioUsuario);
+console.log(typeof loginUsuario);
+console.log(typeof contaUsuario);
+console.log(typeof statusUsuario);
+
+
+statu
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
