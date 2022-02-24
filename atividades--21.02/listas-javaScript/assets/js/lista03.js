0c143bdf93d840126beb95d901dc5be86337ab5c
Tetola-17 <vhpzakimi@hotmail.com>
2022-02-23 22:22:47 -0300

    ainda fazendo a lista 3

--- atividades--21.02/listas-javaScript/assets/js/lista03.js
+++ atividades--21.02/listas-javaScript/assets/js/lista03.js
@@ -1,4 +1,54 @@
 
+console.log('-----------------------------------------');
+
+//////////  .1.
+
+console.log(`Escreva 5 nº`);
+
+let numero = 3;
+let vezes = 0;
+let numerosMaiores = 0;
+
+
+if (vezes <=5){
+
+    console.log(`Digite o ${vezes}º numero`);
+
+    let numero; //PEGA O NUMERO AKI
+    if (numero >=10){
+        numerosMaiores+1;
+    }
+    vezes+1;
+    console.log(`${vezes}º numero: ${numero}`);
+}
+
+console.log(`Dos numeros digitados, ${numerosMaiores} deles são maiores que 10`);
+
+
+console.log('-----------------------------------------');
+
+//////////  .2.
+
+let n1 = 3;
+let n2 = 5;
+let n3 = 9;
+let n4 = 8;
+let n5 = 1;
+let maiores = 0;
+
+console.log('Escreva 5 valores inteiros');
+
+
+let media;
+media = n1 + n2 + n3 + n4 + n5 / 4;
+
+if (n1 >= media){numero+1};
+if (n2 >= media){numero+1};
+if (n3 >= media){numero+1};
+if (n4 >= media){numero+1};
+if (n5 >= media){numero+1};
+
+console.log('A média entre os números é: ');
 
 
 
