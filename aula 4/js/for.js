ed10d760c93c470d26e1fd10cd3420481768c683
Tetola-17 <vhpzakimi@hotmail.com>
2022-03-14 09:50:08 -0300

    aula 4

---
+++ aula 4/js/for.js
@@ -0,0 +1,25 @@
+
+
+
+
+// fazer uma contagem de 0 até 20
+
+for(let i=0; i <= 20; i++){  // inicio; até ser igual menor que; passo(add cada vez)
+
+    console.log(i);
+
+}
+
+
+console.log('------------------------------------------');
+
+
+// sorteio de 10 numeros                
+
+for(let i=0; i <=9; i++){
+    let numeroSorteado = Math.round(Math.random() * 100)
+    console.log(`número sorteado = ${numeroSorteado}`);
+
+}
+
+
